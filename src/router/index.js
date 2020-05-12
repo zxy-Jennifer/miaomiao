30d56b9e8309ca98c46079e2428333e0085cd1ca
zxy_Jennifer <jsxzpxzxy@163.com>
2020-05-12 23:53:37 +0800

    组件的创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,21 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    movieRouter,
+    cinemaRouter,
+    mineRouter,
+    {
+      path: '/*',
+      redirect: '/movie'
+    }
+  ]
 })
-
-export default router
